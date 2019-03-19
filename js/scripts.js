0703b9a787512ff046481638534df5fa9345f071
kkhriapak <45441701+kkhriapak@users.noreply.github.com>
2019-03-19 10:36:03 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+console.log('Welcome to my portfolio site!')
+
+$(document).ready(function() {
+  $('.project-preview').mouseenter(function() {
+    $(this).children('.project-details').css('opacity', '1')
+    $(this).children('.fade').children('.project-image').css('opacity', '0.3')
+  })
+  $('.project-preview').mouseleave(function() {
+    $(this).children('.project-details').css('opacity', '0')
+    $(this).children('.fade').children('.project-image').css('opacity', '1')
+  })
+})
